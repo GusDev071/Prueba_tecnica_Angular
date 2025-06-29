29867279995223c024708b1df36c6cd087e33f16
GusDev071 <gustavofc3441@gmail.com>
2025-06-29 02:43:39 -0600

    Proyecto finalizado con dist

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8bO2dinpA50w1HOb8GpQwVqKpjSmjkmo",
+  authDomain: "prueba-frontend-b3b8a.firebaseapp.com",
+  projectId: "prueba-frontend-b3b8a",
+  storageBucket: "prueba-frontend-b3b8a.firebasestorage.app",
+  messagingSenderId: "51794843129",
+  appId: "1:51794843129:web:dcdcf402520cc06cb69928"
+};
+
+
+export const firebaseApp = initializeApp(firebaseConfig);
